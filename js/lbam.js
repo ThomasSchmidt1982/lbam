ba98ed2daa8105ab378558d77cd4e904e2c479ce
Thomas Schmidt <thomas.michel.schmidt@gmail.com>
2025-12-15 15:48:17 +0100

    reponsive menu

--- js/lbam.js
+++ js/lbam.js
@@ -1,8 +1,30 @@
 
 fetch('./html/nav.html')
     .then(res => res.text())
-    .then(html => document.getElementById('nav').innerHTML = html )
+    .then(html => {
+        document.getElementById('nav').innerHTML = html;
+        const burger = document.getElementById('burger');
+        if (burger) {
+            burger.addEventListener('click', showResponsiveMenu);
+        }
+    } );
 
 fetch('./html/footer.html')
     .then(res => res.text())
     .then(html => document.getElementById('footer').innerHTML = html )
+
+
+function showResponsiveMenu(){
+    var menu = document.getElementById("responsive_menu");
+    var icon = document.getElementById("burger");
+
+    if (menu.className === "") {
+        menu.className = "open";
+        icon.className = "open";
+        document.body.style.overflowY = "hidden";
+    } else {
+        menu.className = "";
+        icon.className = "";
+        document.body.style.overflowY = "";
+    }
+}
